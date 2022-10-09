47a05cae0fd3e3efc27af426eff59a3cb3a586ba
ismailwaxer <ismailwaxir@gmail.com>
2022-10-09 14:15:26 +0500

    module uploaded

---
+++ gt_woocommerce_integration/models/product_attribute.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Globalteckz
+#    Copyright (C) 2012 (http://www.globalteckz.com)
+#
+##############################################################################
+from odoo import api, fields, models, _
+
+
+class product_attribute(models.Model):
+    _inherit = 'product.attribute'
+
+    woocom_id = fields.Char(string='Woocommerce Id')
+    prod_attribute = fields.One2many('product.attribute.value', 'attribute_id', string='attribute value')
+    shop_ids = fields.Many2many('sale.shop', 'attr_shop_rel', 'attr_id', 'shop_id', string="Shop")
+    company_id = fields.Many2one('res.company', string='Company', index=True)
+
+
+class product_attribute_value(models.Model):
+    _inherit = "product.attribute.value"
+
+    woocom_id = fields.Char(string='Woocommerce Id')
+    company_id = fields.Many2one('res.company', string='Company', index=True)
+
+
+# class product_category(models.Model):
+#     _inherit = "product.category"
+# #
+#     company_id = fields.Many2one('res.company', string='Company', index=True)
+#     woocom_id = fields.Char(string='Woocommerce Id')
+#     shop_id = fields.Many2one('sale.shop', 'Shop ID')
+#     shop_ids = fields.Many2many('sale.shop', 'categ_shop_rel', 'categ_id', 'shop_id', string="Shop")
+#     to_be_exported = fields.Boolean(string="To be exported?")
+#     
+#     
+#     @api.multi
+#     def get_leaf(self):
+#         for leaf in self:
+#             c_ids = self.search([('parent_id','=',leaf.id)])
+#             if not c_ids:
+#                 leaf.leaf_cat = True
+# 
+#     woocom_id = fields.Char(string='Woocommerce Id')
+#     write_date = fields.Datetime(string="Write Date")
+#     shop_id = fields.Many2one('sale.shop', 'Shop ID')
+#     shop_ids = fields.Many2many('sale.shop', 'categ_shop_rel', 'categ_id', 'shop_id', string="Shop")
+#     leaf_cat = fields.Boolean(compute="get_leaf",string="Leaf Category", default=False)
+# #     to_be_exported = fields.Boolean(string="To be exported?")
